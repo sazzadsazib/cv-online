a962f9e3450bdcf16a17fbd0549274af41e5da5f
Sazzad Sazib <sazib66@gmail.com>
2020-03-06 03:35:31 +0600

    feat: added loader

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,35 @@
 <template>
-    <cvContainer v-bind:cvData="cvData" />
+    <div>
+        <loader v-if="loading" />
+        <cvContainer v-if="!loading" v-bind:cvData="cvData" />
+    </div>
 </template>
 
 <script>
 import cvContainer from './components/cvContainer.vue';
 import cvData from './cvdata.js';
+import loader from './components/elements/loader';
 
 export default {
     title: 'Sazib CV Online',
     name: 'App',
     components: {
         cvContainer,
+        loader,
     },
     data() {
         return {
             cvData: cvData,
+            loading: true,
             default: () => {},
         };
+    },
+    mounted() {
+        this.$nextTick(() => {
+            setTimeout(()=>{
+                this.loading = false;
+            },1500)
+        });
     },
 };
 </script>
---
+++ src/components/elements/loader.vue
@@ -0,0 +1,65 @@
+<template>
+    <div class="loader-container">
+        <div>
+            <i class="loader"></i>
+        </div>
+        <p class="loader__description">Loading Sazzad's CV</p>
+    </div>
+</template>
+<script>
+export default {
+    name: 'Loader',
+};
+</script>
+
+<style scoped>
+.loader {
+    transform: scale(var(--ggs, 1));
+}
+.loader,
+.loader::after,
+.loader::before {
+    box-sizing: border-box;
+    position: relative;
+    display: block;
+    width: 60px;
+    height: 60px;
+}
+.loader::after,
+.loader::before {
+    content: '';
+    position: absolute;
+    border-radius: 100px;
+}
+.loader::before {
+    animation: spinner 1s cubic-bezier(0.6, 0, 0.4, 1) infinite;
+    border: 8px solid transparent;
+    border-top-color: currentColor;
+}
+.loader::after {
+    border: 9px solid;
+    opacity: 0.2;
+}
+@keyframes spinner {
+    0% {
+        transform: rotate(0deg);
+    }
+    to {
+        transform: rotate(359deg);
+    }
+}
+.loader-container {
+    display: flex;
+    height: 100vh;
+    width: 100%;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+}
+.loader__description {
+    margin-top: 2rem;
+    font-size: 0.8rem;
+    font-weight: 400;
+    letter-spacing: 0.2rem;
+}
+</style>
